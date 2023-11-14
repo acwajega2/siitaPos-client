aae5ef17d19d8182e1f1498182ba83654b5aedc8
Chris Allan Wajega <34956300+acwajega2@users.noreply.github.com>
2023-11-14 10:25:55 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import "./styles.css";
 import SaleForm from "./components/SalesForm";
 import ExpenseForm from "./components/ExpenseForm";
 import { ContextProvider } from "./context/Context";
-
+import { Tab, Tabs } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
     <ContextProvider>
       <div className="App">
         <h1 className="app-title">Expense Tracker</h1>
-        <div className="form-container">
-          <SaleForm />
-          <ExpenseForm />
-        </div>
+        <Tabs defaultActiveKey="sale" id="expense-tracker-tabs">
+          <Tab eventKey="sale" title="Daily Sale">
+            <SaleForm />
+          </Tab>
+          <Tab eventKey="expense" title="Daily Expense">
+            <ExpenseForm />
+          </Tab>
+        </Tabs>
       </div>
     </ContextProvider>
   );
